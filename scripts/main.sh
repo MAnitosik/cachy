4ea8e92e24ada78eade6443614d62804b18b94e7
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2026-07-22 00:08:27 +0300

    mirrorlist

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,3 +1,4 @@
+sudo pacman -Scc
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/cachy/refs/heads/main/etc/pacman.d/mirrorlist | sudo tee /etc/pacman.d/mirrorlist > /dev/null
 sudo pacman -Syyuu
 
