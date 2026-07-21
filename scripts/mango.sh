a3d755693cc507ff1aa74b0eaeae1a95b09f38e3
MAnitosik <86661877+MAnitosik@users.noreply.github.com>
2026-07-21 22:00:10 +0300

    a lot of rework, now basing on gnome

--- scripts/mango.sh
+++ scripts/mango.sh
@@ -1,14 +1,17 @@
-shelly --no-confirm aur install yay-bin
-yay -S --noconfirm --needed mangowm-git
-yay -S --noconfirm --needed rofi foot xdg-desktop-portal-wlr swaybg waybar wl-clip-persist cliphist wl-clipboard wlsunset xfce-polkit swaync pamixer wlr-dpms sway-audio-idle-inhibit-git swayidle dimland-git brightnessctl swayosd wlr-randr grim slurp satty swaylock-effects-git wlogout sox
-mkdir -p ~/.config/mango
-git clone https://github.com/DreamMaoMao/mango-config.git ~/.config/mango
+sudo pacman -S --noconfirm --needed gnome-shell gdm
+sudo pacman -S --noconfirm --needed --asdeps gnome-bluetooth-3.0 gst-plugin-pipewire gst-plugins-good
+sudo pacman -S --noconfirm --needed system-config-printer networkmanager
+sudo systemctl enable gdm
+sudo pacman -S --noconfirm --needed baobab decibels showtime gnome-disk-utility gnome-logs gnome-calculator gnome-console amberol gnome-control-center papers loupe resources simple-scan nautilus
+sudo pacman -S --noconfirm --needed curtail video-trimmer fragments gnome-boxes
+sudo pacman -S --noconfirm --needed gnome-shell-extensions extension-manager gvfs-goa gvfs-dnssd
 
-sudo pacman -S appmenu-gtk-module libdbusmenu-glib
+## CachyOS
+##
+sudo pacman -S --noconfirm --needed appmenu-gtk-module libdbusmenu-glib
 tldr --update
-sudo pacman -S --noconfirm --needed flatpak
 sudo pacman -S --noconfirm --needed fuse2
-
+sudo pacman -S --noconfirm --needed flatpak
 flatpak install -y flathub it.mijorus.gearlever
 
 sudo tee -a /etc/default/limine > /dev/null <<'EOF'
@@ -21,38 +24,65 @@ sudo pacman -S --noconfirm --needed cachyos-gaming-meta
 sudo pacman -S --noconfirm --needed gamescope goverlay mangohud lib32-mangohud
 sudo pacman -S --noconfirm --needed heroic-games-launcher-bin steam
 yay -S --noconfirm --needed hydra-launcher-bin
-sudo pacman -S --noconfirm --needed wine-cachyos
-sudo systemctl disable --now ananicy-cpp
+sudo pacman -S --noconfirm --needed proton-cachyos-slr wine-cachyos
+mkdir -p ~/.config/environment.d/
+curl -fsSL https://raw.githubusercontent.com/MAnitosik/cachy/refs/heads/main/config/environment.d/gaming.conf | sudo tee ~/.config/environment.d/gaming.conf > /dev/null
 
-yay -S --noconfirm --needed jdk-temurin elyprismlauncher-bin
-
-sudo pacman -S --noconfirm --needed hip-runtime-amd hiprt
-sudo pacman -S --noconfirm --needed blender godot-mono
+curl -fsSL https://raw.githubusercontent.com/MAnitosik/cachy/refs/heads/main/etc/udev/rules.d/60-ioschedulers.rules | sudo tee /etc/udev/rules.d/60-ioschedulers.rules > /dev/null
 
 sudo pacman -S --noconfirm --needed scx-scheds scx-tools
+sudo systemctl disable --now ananicy-cpp
+sudo mkdir -p /etc/scx_loader/
+curl -fsSL https://raw.githubusercontent.com/MAnitosik/cachy/refs/heads/main/etc/scx_loader/config.toml | sudo tee /etc/scx_loader/config.toml > /dev/null
+sudo systemctl enable --now scx_loader.service
+##
+## CachyOS
+
+flatpak install -y flathub io.github.wartybix.Constrict
+
+sudo pacman -S --noconfirm --needed yay-bin
+
+sudo pacman -S --noconfirm --needed profile-sync-daemon
+sudo systemctl enable --now psd
+sudo systemctl disable --now systemd-oomd
+sudo pacman -S --noconfirm --needed bpftune-git
+sudo systemctl enable --now bpftune
+sudo pacman -S --noconfirm --needed bluez
+sudo systemctl enable --now bluetooth
+sudo systemctl disable --now arch-update arch-update-tray
 
 sudo systemctl disable --now power-profiles-daemon
 sudo pacman -Rns --noconfirm power-profiles-daemon
 sudo pacman -S --noconfirm --needed cpupower
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/cachy/refs/heads/main/etc/default/cpupower-service.conf | sudo tee /etc/default/cpupower-service.conf > /dev/null
 sudo systemctl enable --now cpupower
-sudo mkdir -p /etc/scx_loader/
-curl -fsSL https://raw.githubusercontent.com/MAnitosik/cachy/refs/heads/main/etc/scx_loader/config.toml | sudo tee /etc/scx_loader/config.toml > /dev/null
-sudo systemctl enable --now scx_loader.service
-sudo pacman -S bpftune-git
-sudo systemctl enable --now bpftune
-curl -fsSL https://raw.githubusercontent.com/MAnitosik/cachy/refs/heads/main/etc/udev/rules.d/60-ioschedulers.rules | sudo tee /etc/udev/rules.d/60-ioschedulers.rules > /dev/null
-mkdir -p ~/.config/environment.d/
-curl -fsSL https://raw.githubusercontent.com/MAnitosik/cachy/refs/heads/main/config/environment.d/gaming.conf | sudo tee ~/.config/environment.d/gaming.conf > /dev/null
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/cachy/refs/heads/main/etc/systemd/resolved.conf.d/resolved.conf | sudo tee /etc/systemd/resolved.conf.d/resolved.conf > /dev/null
 curl -fsSL https://raw.githubusercontent.com/MAnitosik/cachy/refs/heads/main/etc/hosts | sudo tee /etc/hosts > /dev/null
 
-sudo pacman -S --noconfirm --needed zed github-cli resources amberol gimp onlyoffice-bin mpv obs-studio obs-studio-plugin-browser video-trimmer
-sudo pacman -S --noconfirm --needed telegram-desktop torbrowser-launcher lact gnome-boxes gnome-firmware pycharm-community-edition
-yay -S --noconfirm --needed brave-origin-bin
-flatpak install -y flathub io.github.kolunmi.Bazaar
-flatpak install -y flathub io.github.pol_rivero.github-desktop-plus
-yay -S --noconfirm --needed ventoy-bin amneziavpn-bin
+
+sudo pacman -S --noconfirm --needed bazaar chromium
+sudo pacman -S --noconfirm --needed cachyos-firefox-settings
+
+sudo pacman -S --noconfirm --needed zed github-cli pycharm-community-edition
+flatpak install -y flathub org.desktop_plus.desktop-plus
+
+sudo pacman -S --noconfirm --needed --asdeps obs-studio-plugin-browser
+sudo pacman -S --noconfirm --needed obs-studio gimp
+yay -S --noconfirm --needed photogimp
+flatpak install -y flathub org.onlyoffice.desktopeditors
+
+sudo pacman -S --noconfirm --needed telegram-desktop vesktop-bin torbrowser-launcher lact gnome-firmware
+sudo pacman -S --noconfirm --needed ventoy-bin
+yay -S --noconfirm --needed amneziavpn-bin
 sudo sh -c "$(curl -Ls https://github.com/v2rayA/v2rayA-installer/raw/main/installer.sh)" @ --with-xray
 sudo systemctl enable --now v2raya
 sudo systemctl enable --now lactd
+
+
+yay -S --noconfirm --needed --asdeps jdk-temurin
+yay -S --noconfirm --needed elyprismlauncher-bin
+flatpak install -y flathub org.vinegarhq.Sober org.vinegarhq.Vinegar
+sudo pacman -S --noconfirm --needed --asdeps hip-runtime-amd hiprt
+sudo pacman -S --noconfirm --needed blender godot-mono
+
+echo "use cachyos-rate-mirrors sometimes, also dont enable vulkan shaders pre-caching in steam settings"
